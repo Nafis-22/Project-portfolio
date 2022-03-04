ce80ddd3bdaa872e0ef491099603fb7af9d62003
Nafis-22 <98020183+Nafis-22@users.noreply.github.com>
2022-03-04 11:48:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.carousel').carousel();
+responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
